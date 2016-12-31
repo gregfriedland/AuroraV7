0229664c6575062cb86374dd7bd0a36a78be60b6
Greg Friedland <greg.friedland@gmail.com>
2016-12-31 00:35:23 -0700

    bzr: add custom width/height

--- src/Bzr.cpp
+++ src/Bzr.cpp
@@ -6,7 +6,8 @@
 #include "Util.h"
 
 #define BZR_SPEED_MULTIPLIER 100
-
+#define MIN_WIDTH 192
+#define MIN_HEIGHT 96
 
 BzrDrawer::BzrDrawer(int width, int height, int palSize, Camera* camera)
 : Drawer("Bzr", width, height, palSize), m_colorIndex(0), m_camera(camera) {
@@ -16,16 +17,19 @@ BzrDrawer::BzrDrawer(int width, int height, int palSize, Camera* camera)
     m_settings.insert(std::make_pair("params",0));
     m_settingsRanges.insert(std::make_pair("speed", std::make_pair(10,100)));
     m_settingsRanges.insert(std::make_pair("colorSpeed", std::make_pair(0,50)));
-    m_settingsRanges.insert(std::make_pair("zoom", std::make_pair(30,150)));
-    m_settingsRanges.insert(std::make_pair("params", std::make_pair(0,0)));
+    m_settingsRanges.insert(std::make_pair("zoom", std::make_pair(30,100)));
+    m_settingsRanges.insert(std::make_pair("params", std::make_pair(0,7)));
+
+    m_bzrWidth = std::max(MIN_WIDTH, m_width);
+    m_bzrHeight = std::max(MIN_HEIGHT, m_height);
 
     m_q = 0;
     m_state = 0;
 
     for (size_t q = 0; q < 2; ++q) {
-        m_a[q] = new Array2D<float>(width, height);
-        m_b[q] = new Array2D<float>(width, height);
-        m_c[q] = new Array2D<float>(width, height);
+        m_a[q] = new Array2D<float>(m_bzrWidth, m_bzrHeight);
+        m_b[q] = new Array2D<float>(m_bzrWidth, m_bzrHeight);
+        m_c[q] = new Array2D<float>(m_bzrWidth, m_bzrHeight);
     }
 
     reset();
@@ -40,9 +44,9 @@ BzrDrawer::~BzrDrawer() {
 }
 
 void BzrDrawer::reset() {
-    for (int x = 0; x < m_width; x++) {
-        for (int y = 0; y < m_height; y++) {
-            int index = x + y * m_width;
+    for (int x = 0; x < m_bzrWidth; x++) {
+        for (int y = 0; y < m_bzrHeight; y++) {
+            int index = x + y * m_bzrWidth;
             m_a[m_q]->get(index) = randomFloat(0, 1);
             m_b[m_q]->get(index) = randomFloat(0, 1);
             m_c[m_q]->get(index) = randomFloat(0, 1);
@@ -51,25 +55,46 @@ void BzrDrawer::reset() {
 
     switch (m_settings["params"]) {
         case 0:
-            m_ka = 1.3;
+            m_ka = 0.5;
+            m_kb = 0.5;
+            m_kc = 0.6;
+            break;
+        case 1:
+            m_ka = 1.1;
             m_kb = 1.1;
             m_kc = 0.9;
             break;
-        case 1:
+        case 2:
             m_ka = 0.9;
             m_kb = 1.0;
             m_kc = 1.1;
             break;
-        case 2:
+        case 3:
             m_ka = 0.9;
             m_kb = 0.9;
             m_kc = 1.1;
             break;
-        case 3:
+        case 4:
             m_ka = 1;
             m_kb = 1;
             m_kc = 1.1;
             break;
+        case 5:
+            m_ka = 1;
+            m_kb = 1;
+            m_kc = 1;
+            break;
+        case 6:
+            m_ka = 0.5;
+            m_kb = 0.5;
+            m_kc = 0.5;
+            break;
+        case 7:
+            m_ka = 0.75;
+            m_kb = 0.75;
+            m_kc = 0.75;
+            break;
+
     }
 
     std::cout << "Bzr with param set #" << m_settings["params"] <<
@@ -77,8 +102,8 @@ void BzrDrawer::reset() {
 }
 
 void BzrDrawer::draw(int* colIndices) {
-    float speed = 1; //m_settings["speed"] / 100.0;
-    float zoom = 1; //m_settings["zoom"] / 100.0;
+    float speed = m_settings["speed"] / 100.0;
+    float zoom = m_settings["zoom"] / 100.0;
 
     int numStates = BZR_SPEED_MULTIPLIER - std::floor(std::pow(speed, 0.25) * (BZR_SPEED_MULTIPLIER-1));
 
@@ -86,15 +111,15 @@ void BzrDrawer::draw(int* colIndices) {
         m_state = 0;
 
     if (m_state == 0) {
-        for (int y = 0; y < m_height; ++y) {
-            for (int x = 0; x < m_width; ++x) {
+        for (int y = 0; y < m_bzrHeight; ++y) {
+            for (int x = 0; x < m_bzrWidth; ++x) {
                 float c_a=0, c_b=0, c_c=0;
 
                 for (int j=y-1; j<=y+1; j++) {
-                    int jj = (j + m_height) % m_height;
+                    int jj = (j + m_bzrHeight) % m_bzrHeight;
                     for (int i=x-1; i<=x+1; i++) {
-                        size_t ii = (i + m_width) % m_width;
-                        size_t ind = ii + jj * m_width;
+                        size_t ii = (i + m_bzrWidth) % m_bzrWidth;
+                        size_t ind = ii + jj * m_bzrWidth;
                         c_a += m_a[m_q]->get(ind);
                         c_b += m_b[m_q]->get(ind);
                         c_c += m_c[m_q]->get(ind);
@@ -105,7 +130,7 @@ void BzrDrawer::draw(int* colIndices) {
                 c_b /= 9;
                 c_c /= 9;
 
-                size_t ind = x + y * m_width;
+                size_t ind = x + y * m_bzrWidth;
                 m_a[1 - m_q]->get(ind) = std::min(std::max(c_a + c_a * ( m_ka * c_b - m_kc * c_c ), 0.0f), 1.0f);
                 m_b[1 - m_q]->get(ind) = std::min(std::max(c_b + c_b * ( m_kb * c_c - m_ka * c_a ), 0.0f), 1.0f);
                 m_c[1 - m_q]->get(ind) = std::min(std::max(c_c + c_c * ( m_kc * c_a - m_kb * c_b ), 0.0f), 1.0f);
@@ -118,7 +143,7 @@ void BzrDrawer::draw(int* colIndices) {
         for (size_t x = 0; x < m_width; x++) {
             size_t x2 = x * zoom;
             size_t y2 = y * zoom;
-            size_t ind = x2 + y2 * m_width;
+            size_t ind = x2 + y2 * m_bzrWidth;
             float aNext = m_a[m_q]->get(ind);
             float aPrev = m_a[1 - m_q]->get(ind);
 
--- src/Bzr.h
+++ src/Bzr.h
@@ -16,6 +16,7 @@ public:
     virtual void draw(int* colIndices);
 
  private:
+ 	size_t m_bzrWidth, m_bzrHeight;
     int m_q, m_state;
     Array2D<float> *m_a[2], *m_b[2], *m_c[2];
     float m_ka, m_kb, m_kc;
